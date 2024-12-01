1e9eb68f9fa1b77dce15fe3befb7ec32dff42d08
Daan Sieben <developer@daansieben.net>
2024-12-01 13:53:05 +0100

    first commit

---
+++ aoc-core/src/lib.rs
@@ -0,0 +1,13 @@
+mod answer;
+pub use answer::*;
+mod puzzle;
+pub use puzzle::*;
+mod solution_collection;
+pub use solution_collection::*;
+mod solution_wrapper;
+pub use solution_wrapper::*;
+mod timer;
+
+extern crate aoc_procmacro;
+pub use aoc_procmacro::{aoc, aoc_puzzle};
+pub use aoc_procmacro_internals::public::*;
